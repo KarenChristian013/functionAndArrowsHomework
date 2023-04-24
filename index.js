0ec79e761bb507ab60183b3af86628f7affc2926
KarenChristian <karen.lewis013@yahoo.com>
2023-04-23 20:23:36 -0400

    functionsAndArrows

---
+++ index.js
@@ -0,0 +1,46 @@
+var a = 10
+var b = 5
+
+function add (num1, num2){
+    return (num1 + num2);
+} 
+    console.log(add(a,b));
+
+function sub(num1, num2){
+    return (num1 - num2);
+} 
+    console.log(sub(a,b));
+
+function multiply(num1, num2) {
+    return (num1 * num2)
+}
+    console.log(multiply(a, b));
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+    console.log(divide(a, b));
+
+var result = (a,b) => (a + b);
+    console.log(result(a,b));
+
+var result = (a,b) => (a - b);
+    console.log(result(a,b));
+
+var result = (a,b) => (a * b);
+    console.log(result(a,b));
+
+var result = (a,b) => (a / b);
+    console.log(result(a,b));
+
+
+
+       
+  
+
+
+
+
+
+
+
